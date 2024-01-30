2de02d4de6695ff49473d10154b35708fb2eb466
henrfos <henrik.fossing17@gmail.com>
2024-01-30 15:23:00 +0100

    temporary js code for word scramble

--- js/word-scramble.js
+++ js/word-scramble.js
@@ -1,15 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const words = ['hangman', 'javascript', 'bootstrap', 'developer', 'programming'];
-    let selectedWord = '';
-    let guessedWord = [];
-    let incorrectGuesses = 0;
+    const words = ['javascript', 'programming', 'web', 'developer', 'scramble'];
+    let scrambledWord = '';
+    let originalWord = '';
 
     function startGame() {
-        selectedWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
-        guessedWord = Array(selectedWord.length).fill('_');
-        incorrectGuesses = 0;
-        updateHangmanImage();
-        updateWordToGuess();
+        originalWord = words[Math.floor(Math.random() * words.length)];
+        scrambledWord = scrambleWord(originalWord);
+        updateScrambledWord();
     }
 
+    function scrambleWord(word) {
+        const wordArray = word.split('');
+        for (let i = wordArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+        }
+        return wordArray.join('');
+    }
+
+    function updateScrambledWord() {
+        const scrambledWordElement = document.getElementById('scrambledWord');
+        scrambledWordElement.textContent = scrambledWord;
+    }
+
+    function handleGuess() {
+        const userInput = document.getElementById('userInput').value.toLowerCase();
+        if (userInput === originalWord) {
+            alert('Congratulations! You guessed the word.');
+            startGame();
+        } else {
+            alert('Incorrect. Try again!');
+        }
+        document.getElementById('userInput').value = '';
+    }
+
+    startGame();
+
+    const submitBtn = document.getElementById('submitBtn');
+    submitBtn.addEventListener('click', handleGuess);
 });
