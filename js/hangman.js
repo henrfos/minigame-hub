fd97c61fe0b3b1054285249699ab4f2f5e046f1d
henrfos <henrik.fossing17@gmail.com>
2024-03-14 23:01:33 +0100

    temporary js code

--- js/hangman.js
+++ js/hangman.js
@@ -1,3 +1,40 @@
-for (let i = 97; i < 122; i++){
-    console.log(String.fromCharCode(i));  
+const keyboardDiv = document.querySelector(".keyboard");
+const wordDisplay = document.querySelector(".letters");
+const guessesText = document.querySelector(".guesses b");
+const hangmanImage = document.querySelector(".hangman-container img");
+
+let currentWord, wrongGuessCount = 0;
+const maxGuesses = 6;
+
+const getRandomWord = () => {
+    const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    console.log(word);
+    document.querySelector(".hint b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
+
+const initGame = (button, clickedLetter) => {
+    if(currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) =>{
+            if(letter === clickedLetter){
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed")
+            }
+        })
+    } else {
+        wrongGuessCount++;
+        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`
+    }
+    button.disabled = true;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`
+}
+
+for (let i = 97; i < 122; i++){
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+};
+
+getRandomWord();
---
+++ js/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word: "javascript",
+        hint: "Most popular programming language in the world"
+    },
+    {
+        word: "norway",
+        hint: "Country in scandinavia"
+    },
+    {
+        word: "dog",
+        hint: "A popular pet"
+    },
+    {
+        word: "italy",
+        hint: "Pizza"
+    },
+    {
+        word: "coffe",
+        hint: "Beverage popular in the mornings"
+    },
+];
